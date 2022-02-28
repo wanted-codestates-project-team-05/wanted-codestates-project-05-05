6787043265a094b394df791d68137ab288b4a061
kyk1211 <85268135+kyk1211@users.noreply.github.com>
2022-02-28 21:49:42 +0900

    [client]/edit: textHighlight (#3)
    * feat: app.js route && texthighlight component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 import './reset.css';
-import Search from './component/home/Search';
+import { Routes, Route } from 'react-router-dom';
+import Home from './component/home/Home';
+import SearchUrl from './component/searchUrl/SearchUrl';
+import SearchKeyword from './component/searchKeyword/SearchKeyword';
 
 function App() {
-  return <Search />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="searchKeyword" element={<SearchKeyword />} />
+      <Route path="searchUrl" element={<SearchUrl />} />
+    </Routes>
+  );
 }
 
 export default App;
