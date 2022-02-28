243f272874c8da4585fa611cdbb5ca4b4f446497
곽윤구 <dbsrndi123@naver.com>
2022-02-28 17:48:33 +0900

    feat: app.js route && texthighlight component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import './reset.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './component/home/Home';
+import SearchUrl from './component/searchUrl/SearchUrl';
+import SearchKeyword from './component/searchKeyword/SearchKeyword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="searchKeyword" element={<SearchKeyword />} />
+      <Route path="searchUrl" element={<SearchUrl />} />
+    </Routes>
   );
 }
 
