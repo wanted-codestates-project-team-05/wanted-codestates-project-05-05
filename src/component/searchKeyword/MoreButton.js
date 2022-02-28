b6994e941b69cc3acd1ac56071c6565954cc53d5
kyu <2066apple@naver.com>
2022-02-28 23:16:20 +0900

    Feat: 더보기 버튼에 로딩효과 추가

--- src/component/searchKeyword/MoreButton.js
+++ src/component/searchKeyword/MoreButton.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { TailSpin } from 'react-loader-spinner';
 
 export const MoreButton = (props) => {
 
 	const { cardNum, setCardNum } = props;
+	const [buttonLoading, setButtonLoading] = useState(true);
 	const perContents = 30;
 
 	const handleMoreButton = () => {
-		setCardNum(cardNum + perContents)
+		setTimeout(() => {
+			setButtonLoading(true)			
+			setCardNum(cardNum + perContents)
+		}, 1000)
+		setButtonLoading(false)
 	}
 
 	return (
-		<Container onClick={handleMoreButton}>
-			More
+		<Container onClick={handleMoreButton} loading={buttonLoading}>
+			{buttonLoading ? 'More' : <TailSpin color="#4b4b4b" height={30} width={30} />}
 		</Container>
 	)
 }
@@ -21,7 +27,7 @@ const Container = styled.div`
 	width: 9.375rem;
 	height: 3.125rem;
 	margin: 0 auto;
-	background-color: lightblue;
+	background-color: ${(props) => props.loading ? 'lightblue' : 'lightgray'};
 	border-radius: 5px;
 	display: flex;
 	justify-content: center;
