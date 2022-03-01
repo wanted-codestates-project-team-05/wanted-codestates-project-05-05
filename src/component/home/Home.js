92f30a7c184b7b1d6734a7d2274931285178f191
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-01 15:21:35 +0900

    Feat: 검색 로딩 처리, 에러처리

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -11,68 +11,100 @@ const Home = () => {
   const [result, setResult] = useLocalStorage('result', '');
   const [inputValue, setInputValue] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const navigate = useNavigate();
   const handleChangeInput = (e) => {
     const { value } = e.target;
     setInputValue(value);
   };
-  const handleError = () => {
-    if (result.length === 0) {
-      console.log('검색결과가 없습니다. ');
+  const filterResult = (type, value) => {
+    let result = [];
+    if (type === 'productCode') {
+      result = products.filter((product) => product.product_code === Number(value));
+      setResult(result);
+      return result;
+    } else if (type === 'imageUrl') {
+      result = products.filter((product) => product.image_url === value);
+      setResult(result);
+      return result;
+    } else if (type === 'keyword') {
+      result = products.filter((product) => product.name.includes(value));
+      setResult(result);
+      return result;
+    } else {
+      console.log('타입지정 에러');
     }
   };
-  const filterResult = (type, value) => {
-    switch (type) {
-      case 'productCode':
-        setResult(products.filter((product) => product.product_code === Number(value)));
-        break;
-      case 'imageUrl':
-        setResult(products.filter((product) => product.image_url === value));
-        break;
-      case 'keyword':
-        setResult(products.filter((product) => product.name.includes(value)));
-        break;
-      default:
-        break;
+  const handleError = () => {
+    console.log('검색결과 없음');
+    setIsError(true);
+  };
+  const goSearchUrl = (result, value) => {
+    if (result.length === 0) {
+      handleError();
+    } else {
+      navigate(`searchUrl/:${value}`);
+    }
+  };
+  const goSearchKeyword = (result, value) => {
+    if (result.length === 0) {
+      handleError();
+    } else {
+      navigate(`searchKeyword/:${value}`);
     }
   };
   const matchingSearchType = async (value) => {
     const productCodeCheck = /^[0-9]*$/;
     const imageUrlCheck = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?(.*?)\.(jpg|jpeg|png|gif|bmp|pdf)$/;
     if (productCodeCheck.test(value)) {
-      await filterResult('productCode', value);
-      await navigate(`searchUrl/:${value}`);
+      const result = filterResult('productCode', value);
+      goSearchUrl(result, value);
     } else if (imageUrlCheck.test(value)) {
-      await filterResult('imgUrl', value);
+      const result = filterResult('imageUrl', value);
       value = value.replace(/\/|:/g, '_');
-      await navigate(`searchUrl/:${value}`);
+      goSearchUrl(result, value);
     } else {
-      await filterResult('keyword', value);
-      await navigate(`searchKeyword/:${value}`);
+      const result = filterResult('keyword', value);
+      goSearchKeyword(result, value);
     }
   };
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     setQuery(inputValue);
-    matchingSearchType(inputValue);
-    setInputValue('');
+    setTimeout(() => {
+      setIsLoading(false);
+      matchingSearchType(inputValue);
+      setInputValue('');
+    }, 1000);
   };
 
   return (
     <Wrapper>
       <Header />
       {isLoading ? (
-        <Loading message="메인 페이지 로딩중" />
+        <Loading message="Loading data..." />
       ) : (
         <Main>
-          <Sign>
-            <h1>Artificial Intelligence</h1>
-            <h1>
-              PXL
-              <span> Fashion </span>
-              Viewer
-            </h1>
-          </Sign>
+          {isError ? (
+            <Sign>
+              <h1>No results found...</h1>
+              <Des>
+                <li>product 코드 검색: 숫자만 입력(ex: 1)</li>
+                <li>url 검색: url 입력(ex: https://static.pxl.ai/problem/images/VT-070.jpg)</li>
+                <li>키워드 검색 : 키워드 입력 (ex: 원피스)</li>
+              </Des>
+            </Sign>
+          ) : (
+            <Sign>
+              <h1>Artificial Intelligence</h1>
+              <h1>
+                PXL
+                <span> Fashion </span>
+                Viewer
+              </h1>
+            </Sign>
+          )}
           <Form onSubmit={handleSearch}>
             <Input type="text" placeholder="IMAGE URL or KEYWORD" onChange={handleChangeInput} value={inputValue} />
             <Btn type="submit">검색</Btn>
@@ -145,6 +177,12 @@ const Btn = styled.button`
   font-weight: 600;
   cursor: pointer;
   color: #4b4b4b;
+`;
+
+const Des = styled.ul`
+  font-size: 1.5rem;
+  color: grey;
+  list-style: square;
 `;
 
 export default Home;
