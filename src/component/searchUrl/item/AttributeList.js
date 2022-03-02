2afe2f90529730c5a70e78493c6b7cc3598e86d6
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-02 18:03:01 +0900

    Feat: url 및 코드 검색 시 네트워크 요청 없이, localStorage 사용 코드로 변경

--- src/component/searchUrl/item/AttributeList.js
+++ src/component/searchUrl/item/AttributeList.js
@@ -1,74 +1,56 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Loading from '../../common/Loading';
-
-const regionDataUrl = 'https://static.pxl.ai/problem/data/regions.json';
+import useLocalStorage from '../../../hooks/useLocalStorage';
 
 const AttributeList = () => {
   let [searchParams, setSearchParams] = useSearchParams();
   let productCode = searchParams.get('productCode');
-  let imageUrl = searchParams.get('imageUrl');
 
   const [gender, setGender] = useState('');
   const [attributes, setAttributes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const navigate = useNavigate();
-
+  const [allRegions, setAllRegions] = useLocalStorage('regions');
+  const [result, setResult] = useLocalStorage('result');
   useEffect(() => {
-    const getResionData = async () => {
-      setIsLoading(true);
-      await axios
-        .get(regionDataUrl)
-        .then((res) => {
-          if (productCode) {
-            const genderIndex = res.data[productCode - 1].gender.indexOf('.');
-            const genderCheck = res.data[productCode - 1].gender.substring(
-              genderIndex + 1,
-              res.data[productCode - 1].gender.length
-            );
-            setGender(genderCheck);
+    setIsLoading(true);
+    if (productCode) {
+      const genderIndex = allRegions[productCode - 1].gender.indexOf('.');
+      const genderCheck = allRegions[productCode - 1].gender.substring(
+        genderIndex + 1,
+        allRegions[productCode - 1].gender.length
+      );
+      setGender(genderCheck);
 
-            const attributesList = res.data[productCode - 1].attributes.map((data, i) => {
-              let key = Object.keys(data)[0];
-              return (
-                <div key={key} className="container">
-                  <div className="hashtag">#{data[key].toUpperCase()}</div>
-                  <div>{key.toUpperCase()}</div>
-                </div>
-              );
-            });
-            setAttributes(attributesList);
-          } else {
-            const localResult = JSON.parse(window.localStorage.getItem('result'));
-            const genderIndex = localResult[0].gender.indexOf('.');
-            const genderCheck = localResult[0].gender.substring(genderIndex + 1, localResult[0].gender.length);
-            setGender(genderCheck);
+      const attributesList = allRegions[productCode - 1].attributes.map((data, i) => {
+        let key = Object.keys(data)[0];
+        return (
+          <div key={key} className="container">
+            <div className="hashtag">#{data[key].toUpperCase()}</div>
+            <div>{key.toUpperCase()}</div>
+          </div>
+        );
+      });
+      setAttributes(attributesList);
+    } else {
+      const genderIndex = result[0].gender.indexOf('.');
+      const genderCheck = result[0].gender.substring(genderIndex + 1, result[0].gender.length);
+      setGender(genderCheck);
 
-            const attributesList = localResult[0].attributes.map((data, i) => {
-              let key = Object.keys(data)[0];
-              return (
-                <div key={key} className="container">
-                  <div className="hashtag">#{data[key].toUpperCase()}</div>
-                  <div>{key.toUpperCase()}</div>
-                </div>
-              );
-            });
-            setAttributes(attributesList);
-          }
-
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          alert('데이터를 불러오는데 실패하였습니다.');
-          navigate('/');
-        });
-    };
-    getResionData();
+      const attributesList = result[0].attributes.map((data, i) => {
+        let key = Object.keys(data)[0];
+        return (
+          <div key={key} className="container">
+            <div className="hashtag">#{data[key].toUpperCase()}</div>
+            <div>{key.toUpperCase()}</div>
+          </div>
+        );
+      });
+      setAttributes(attributesList);
+    }
+    setIsLoading(false);
     return () => (setGender(null), setAttributes(null));
-    // return () => getResionData.cancel();
   }, [searchParams]);
 
   if (isLoading) return <Loading />;
